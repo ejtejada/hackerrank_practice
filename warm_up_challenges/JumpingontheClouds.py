35d727aa3faa9bddc146d0ea1da236e8c246bf09
ejtejada <edgar_tejada0971@elcamino.edu>
2021-05-04 21:01:50 -0700

    Attempting backtracing greedy solution to Jumping on the Clouds warmup

---
+++ warm_up_challenges/JumpingontheClouds.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+#Based on warm up by HackerRank user Shafaet
+#Author Edgar Tejada
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jumpingOnClouds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY c as parameter.
+#
+
+def jumpingOnClouds(c):
+    # Write your code here
+    #This is a variation of a shortest path problem, where likely the greedy algorithm will NOT always work. 
+    #Possible approach is bad, trying every possible path :/
+    
+    jumps_to_completion = 0; #Initialize output
+    #First iteration, the greedy approach
+    jumpingOnGreed(c):
+    
+    return (jumps_to_completion);
+
+def jumpingOnGreed(c):
+	jumps_to_completion = 0;
+	last_step = 0;
+	bool_death = False;
+	#Current approach, jump greedily unless this causes death, then backtrace to a safer jump
+	for i in range(0,c):
+		if (c[i+2] == 0 and (i+2) <= c and not bool_death):
+			jumps_to_completion = jumps_to_completion + 1;
+			i = i + 1; #Skip a step greedily
+			last_step = 2;
+			bool_death = False;
+		elif((c[i+1] == 0 and (i+1) <= c and not bool_death)
+			jumps_to_completion = jumps_to_completion + 1;
+			last_step = 1;
+			bool_death = False;
+		elif(bool_death):
+			#Next legal moves cause death, go back
+			i = i - 1 - last_step;
+		else: #We landed on a thunderhead and died, backtrace
+			bool_death = Truth;
+	
+	return (jumps_to_completion);
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
