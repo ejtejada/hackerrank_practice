193529144a083e2f3e59425a87379f448ed0b142
ejtejada <edgar_tejada0971@elcamino.edu>
2021-05-04 21:52:13 -0700

    Nope, I'm still stubbornly molding a greedy backtracing solution before doing the obvious BFS

--- warm_up_challenges/JumpingontheClouds.py
+++ warm_up_challenges/JumpingontheClouds.py
@@ -22,31 +22,38 @@ def jumpingOnClouds(c):
     jumps_to_completion = 0; #Initialize output
     #First iteration, the greedy approach
     #print(type(c[0])); #[DELETE_ME] Just a sanity type check 
-    #jumps_to_completion = jumpingOnGreed(c);
+    jumps_to_completion = jumpingOnGreed(c);
     
     return (jumps_to_completion);
 
 #Not an optimal solution, this will fail when greedy algo will fail!
 def jumpingOnGreed(c):
-	jumps_to_completion = 0; 
+	jumps_to_completion = 0; #Hacky offset, not clean 
 	last_step = 0;
 	bool_death = False;
 	#Current approach, jump greedily unless this causes death, then backtrace to a safer jump
 	for i in range(0,len(c)-2):
-		if ((c[i+2] == 0) and (i+2) < len(c) and not bool_death):
+		if(bool_death):
+			print(i);
+			print('We backtraced');
+			#Next legal moves cause death, go back
+			i = i - 1 -  last_step;
+			jumps_to_completion = jumps_to_completion - 1;
+		elif ((c[i] == 0) and (c[i+2] == 0) and (i+2) < len(c) and not bool_death):
+			print(i);
 			jumps_to_completion = jumps_to_completion + 1;
 			i = i + 1; #Skip a step greedily
 			last_step = 2;
 			bool_death = False;
-		elif((c[i+1] == 0) and (i+1) < len(c) and not bool_death):
+			
+		elif((c[i] == 0) and (c[i+1] == 0) and (i+1) < len(c) and not bool_death):
+			print(i);
 			jumps_to_completion = jumps_to_completion + 1;
 			last_step = 1;
 			bool_death = False;
-		elif(bool_death):
-			#Next legal moves cause death, go back
-			i = i - 1 - last_step;
-			jumps_to_completion = jumps_to_completion - 1;
-		else: #We landed on a thunderhead and died, backtrace
+		elif((c[i] == 1)): #We landed on a thunderhead and died, backtrace
+			print(i);
+			print('We died');
 			bool_death = True;
 	
 	return (jumps_to_completion);
